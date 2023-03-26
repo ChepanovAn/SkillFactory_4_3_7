80fc00d3be2e5f0c03f25a7c3d9f825216e3c471
Анатолий <Chepanov199287@gmail.com>
2023-03-26 13:15:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_4_3_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            for (int i = name.Length-1; i>=0; i--)
+            {
+                Console.Write(name[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
